12d3a960d933082008ea287dd73252bacab12bbc
cruel32 <cruel32@nate.com>
2021-07-14 19:41:39 +0900

    barcode test

--- src/components/pages/RootPage.tsx
+++ src/components/pages/RootPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 
 const RootPage = () => {
   console.log('Root Page');
-  return <div>Root Page</div>;
+  return (
+    <div>
+      <BarcodeScannerComponent
+        width={200}
+        height={200}
+        onUpdate={(err, result) => {
+          if (!err) {
+            console.log('result ::::: ', result);
+            alert(result);
+          }
+        }}
+      />
+    </div>
+  );
 };
 export default RootPage;
