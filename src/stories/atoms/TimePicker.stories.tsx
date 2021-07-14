d2069d2a64078f8e60608f1b0a715fd37279ae09
cruel32 <cruel32@nate.com>
2021-07-14 19:50:04 +0900

    에러수정

--- src/stories/atoms/TimePicker.stories.tsx
+++ src/stories/atoms/TimePicker.stories.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Timepicker, { TimePickerProps } from '@/components/atoms/TimePicker';
+import Timepicker, { TimePickerProps } from '@/components/atoms/Timepicker';
 
 export default {
   title: 'Example/Atoms/Timepicker',
