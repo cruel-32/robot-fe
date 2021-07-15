f3c8de8ccda034b5ff43e52ed29e698033c31bca
choiseunghee <mz_choeseunghui@woowafriends.com>
2021-07-15 11:19:12 +0900

    Quagga test

--- src/components/pages/RootPage.tsx
+++ src/components/pages/RootPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 import BarcodeReader from 'react-barcode-reader';
+import Quagga from 'quagga';
 
 const RootPage = () => {
   console.log('Root Page');
@@ -15,6 +16,43 @@ const RootPage = () => {
     alert(e);
   };
 
+  useEffect(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#barcode-reader'), // Or '#yourElement' (optional)
+          width: 320,
+          height: 240,
+        },
+        decoder: {
+          readers: ['code_128_reader'],
+        },
+      },
+      (err: any) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Initialization finished. ::::: Ready to start');
+        Quagga.start();
+        Quagga.onDetected((data: any) => {
+          if (data) console.log('onDetected data ::::: ', data);
+          if (data?.codeResult?.code) alert(data?.codeResult?.code);
+        });
+        Quagga.onProcessed((data: any) => {
+          if (data) console.log('onProcessed data ::::: ', data);
+          if (data?.codeResult?.code) alert(data?.codeResult?.code);
+        });
+        Quagga.decodeSingle({}, (data: any) => {
+          if (data) console.log('decodeSingle data ::::: ', data);
+          if (data?.codeResult?.code) alert(data?.codeResult?.code);
+        });
+      }
+    );
+  }, []);
+
   return (
     <div>
       <BarcodeScannerComponent
@@ -28,6 +66,7 @@ const RootPage = () => {
         }}
       />
       <BarcodeReader onError={onError} onScan={onScan} />
+      <div id="barcode-reader" />
     </div>
   );
 };
